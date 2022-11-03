7d002f09510c177663abd7d2801bb7f7c8cc7de6
Hugo Ernesto <mail.hbler@gmail.com>
2022-11-03 11:02:42 -0300

    feat: model and tests done

--- jobs/models.py
+++ jobs/models.py
@@ -1,5 +1,50 @@
+import uuid
 from django.db import models
 
 
+class Level(models.TextChoices):
+    INTERN = "Estágio"
+    TRAINEE = "Treinee"
+    JUNIOR = "Júnior"
+    MID = "Pleno"
+    SENIOR = "Sênior"
+    DEFAULT = "Não definido"
+    ...
+
+
+class Category(models.TextChoices):
+    FRONT = "Front-End"
+    BACK = "Back-End"
+    FULL = "Full-Stack"
+    DEFAULT = "Não definido"
+    ...
+
+
 class Job(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    url = models.CharField(max_length=254, unique=True)
+    title = models.CharField(max_length=127)
+    period = models.CharField(max_length=15)
+    location = models.CharField(max_length=30)
+    contract = models.CharField(max_length=15)
+    estimated_pay = models.PositiveIntegerField()
+
+    level = models.CharField(
+        max_length=15,
+        choices=Level.choices,
+        default=Level.DEFAULT,
+    )
+
+    category = models.CharField(
+        max_length=15,
+        choices=Category.choices,
+        default=Category.DEFAULT,
+    )
+
+    comapany = models.ForeignKey(
+        "companies.Company",
+        on_delete=models.CASCADE,
+        related_name="jobs",
+    )
+
     ...
