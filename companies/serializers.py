6ca9afdc5bb0b35f355a8d2b4b0dd8077599c675
Matheus Silva <matheus_lsilva@hotmail.com>
2022-11-04 01:21:14 -0300

    feat: company serializer

---
+++ companies/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from companies.models import Company
+
+
+class CompanySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model: Company
+        fields: '__all__'
+        read_only_fields = ["id"]
