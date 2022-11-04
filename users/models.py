0438b23db8fa67f246c3241a843d1a95a3289282
José Duarte <joseduartent@gmail.com>
2022-11-04 14:23:01 -0400

    fix: usermodel fields

--- users/models.py
+++ users/models.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> development
 import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -11,9 +8,7 @@ class User(AbstractUser):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     email = models.CharField(max_length=127, unique=True, null=False)
     password = models.CharField(max_length=127, null=False)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    bio = models.TextField()
+   
 
     REQUIRED_FIELDS = [
         'email',
