5700acd1b7981ac07f0995c99a0ac8aff1a948d7
Hugo Ernesto <mail.hbler@gmail.com>
2022-11-07 02:59:52 -0300

    fix: settings.py

--- manage_project/settings.py
+++ manage_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["gcd-api.herokuapp.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["gdc-api.herokuapp.com", "127.0.0.1", "localhost"]
 
 # Application definition
 DEFAULT_APPS = [
