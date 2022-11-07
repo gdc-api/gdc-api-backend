5332308195a211983438ace5a6dac5cd59f1c153
Hugo Ernesto <mail.hbler@gmail.com>
2022-11-07 15:16:35 -0300

    feat: model and serializer

--- interviews/models.py
+++ interviews/models.py
@@ -1,5 +1,17 @@
+import uuid
+
 from django.db import models
 
 
 class Interview(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    schedule = models.DateTimeField()
+    location = models.TextField(blank=True)
+    was_aprooved = models.BooleanField(blank=True, default=False)
+
+    application = models.ForeignKey(
+        "applications.Application",
+        on_delete=models.CASCADE,
+        related_name="interviews",
+    )
     ...
