fe180ed2eae080290f3d9ae63e5740db5b0838bc
Hugo Ernesto <mail.hbler@gmail.com>
2022-11-08 18:33:04 -0300

    fix: job update url

--- jobs/views.py
+++ jobs/views.py
@@ -7,14 +7,14 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 
 from .models import Job
-from .serializers import JobSerializer
+from .serializers import JobWithoutCompanySerializer
 
 
 class JobDetailView(generics.UpdateAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    serializer_class = JobSerializer
+    serializer_class = JobWithoutCompanySerializer
     lookup_url_kwarg = "job_id"
     queryset = Job.objects.filter()
     ...
