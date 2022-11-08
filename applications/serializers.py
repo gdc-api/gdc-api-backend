9d9dc900557b528b7c421f71ed408d34bbeed48c
Matheus Silva <matheus_lsilva@hotmail.com>
2022-11-08 16:33:52 -0300

    feat: pplications views test

--- applications/serializers.py
+++ applications/serializers.py
@@ -9,7 +9,6 @@ from jobs.serializers import JobSerializer, JobWithoutCompanySerializer
 from companies.models import Company
 from jobs.models import Job
 
-import ipdb
 
 class ApplicationSerializer(serializers.ModelSerializer):
     user = UserSerializer()
@@ -30,22 +29,22 @@ class ApplicationSerializerCreation(serializers.ModelSerializer):
         fields = "__all__"
         read_only_fields = ["created_at", "id"]
 
-    
     def create(self, validated_data):
         company_data = validated_data["job"].pop("company")
         company = Company.objects.create(**company_data)
-        
+
         job_data = validated_data.pop("job")
         job_data["company"] = company
         job = Job.objects.create(**job_data)
-        
+
         application_data = {
-            "user":validated_data["user"],
-            "job":job
+            "user": validated_data["user"],
+            "job": job
         }
 
         application = Application.objects.create(**application_data)
         return application
+
 
 class ApplicationSerializerCreationWithoutCompanySerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
@@ -55,7 +54,7 @@ class ApplicationSerializerCreationWithoutCompanySerializer(serializers.ModelSer
         model = Application
         fields = "__all__"
         read_only_fields = ["created_at", "id"]
-    
+
     def create(self, validated_data):
         company_id = validated_data.pop("company_id")
         user = validated_data["user"]
@@ -64,17 +63,16 @@ class ApplicationSerializerCreationWithoutCompanySerializer(serializers.ModelSer
             company = company.filter(id=company_id).first()
         except Company.DoesNotExist:
             raise NotFound("Company not found")
-        
+
         if not company:
             raise NotFound("Company not found")
-        
+
         job_data = validated_data["job"]
         job = Job.objects.create(**job_data, company=company)
 
-
         application_data = {
-            "user":user,
-            "job":job
+            "user": user,
+            "job": job
         }
 
         application = Application.objects.create(**application_data)
--- applications/urls.py
+++ applications/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path("applications/", views.ListCreateApplicationView.as_view()),
-    path("applications/company/<str:pk>/", views.CreateApplicationWithCompanyIdView.as_view()),
+    path(
+        "applications/company/<str:pk>/",
+        views.CreateApplicationWithCompanyIdView.as_view()
+    ),
     path("applications/<str:pk>/", views.ApplicationDetailView.as_view())
 ]
